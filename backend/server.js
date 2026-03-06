8fa6288345106c5b9ba7d177a77d5debebdb8676
An Lê Văn <anlvhe186705@fpt.edu.vn>
2026-03-06 14:30:26 +0700

    update server.js use env to connect mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dashboard';
+const MONGODB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/dashboard';
 
 // Middleware
 app.use(cors());
