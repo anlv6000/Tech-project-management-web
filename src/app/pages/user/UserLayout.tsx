5b78d5e555556baffe4722e6c1e84ed2c1c213c6
DuongND204 <duongndhe186010@fpt.edu.vn>
2026-03-23 11:52:30 +0700

    add: search bar component

--- src/app/pages/user/UserLayout.tsx
+++ src/app/pages/user/UserLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet, Link, useNavigate, useLocation } from 'react-router';
 import { useAuth } from '../../contexts/AuthContext';
 import { useData } from '../../contexts/DataContext';
 import { API_BASE_URL } from "../../config/baseApi";
+import SearchBar from '../../components/ui/search-bar';
 import {
   CheckCircle,
   LayoutDashboard,
@@ -74,16 +75,7 @@ export default function UserLayout() {
           </Link>
 
           {/* Search */}
-          <div className="flex-1 max-w-md ml-4 hidden md:block">
-            <div className="relative">
-              <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Search projects, tasks..."
-                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
-          </div>
+          <SearchBar />
         </div>
 
         {/* Right section */}
