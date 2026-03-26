ce6d4b107506421016e8a371bad52fb6ada5b27a
Dungohio <124650432+Dungohio@users.noreply.github.com>
2026-03-26 23:36:33 +0700

    fix role of project

--- backend/models/UserProject.js
+++ backend/models/UserProject.js
@@ -1,36 +1,37 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userProjectSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   projectId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Project',
-    required: true
+    ref: "Project",
+    required: true,
   },
   role: {
     type: String,
-    enum: ['projectAdmin', 'pm', 'member', 'viewer'],
-    default: 'member',
+    enum: ["projectAdmin", "projectManager", "member", "viewer"],
+    default: "member",
   },
   joinedAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
-userProjectSchema.set('toJSON', {
+
+userProjectSchema.set("toJSON", {
   transform: (doc, ret) => {
     ret.id = ret._id.toString();
     delete ret._id;
     return ret;
-  }
+  },
 });
 
 // Create compound index to ensure user is only added to project once
 userProjectSchema.index({ userId: 1, projectId: 1 }, { unique: true });
 
-export default mongoose.model('UserProject', userProjectSchema);
+export default mongoose.model("UserProject", userProjectSchema);
